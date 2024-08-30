1ea1463966c36d6c977f9c5618cfc63ce575aa6d
JDeWeese <jdeweese574@icloud.com>
2024-08-29 22:58:56 -0500

    Initial Commit

---
+++ DailyGrind47/DailyGrind47App.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGrind47App.swift
+//  DailyGrind47
+//
+//  Created by Joseph DeWeese on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyGrind47App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
